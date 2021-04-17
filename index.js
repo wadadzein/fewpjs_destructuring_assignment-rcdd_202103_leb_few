2665359d07b0a13f18af7767f387a34b1b9965c7
wadad zein <wadad_zein@hotmail.com>
2021-04-17 23:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 const [red, orange, yellow, green, blue, indigo, violet]= colors;
 const[r, o, y, g, b, , v]= colors;
 const [ , , , , , indg, ]=colors;
+
+//Objects
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
