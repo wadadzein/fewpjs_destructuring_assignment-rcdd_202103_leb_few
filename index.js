bb33a44999a1c55df815f3d9ee96f3dc2960836e
wadad zein <wadad_zein@hotmail.com>
2021-04-17 23:14:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const animalSounds = {
+ moo: 'cow' ,
+ neigh: 'horse',
+ baa: 'sheep' ,
+  oink: 'pig', 
+  cluck: 'chicken'
+  
+};
+const {moo, neigh, baa, oink, cluck} = animalSounds;
 
+const animalNames = {
+ bessie: 'cow' ,
+ //bolt : 'horse',
+ dolly: 'sheep' ,
+  babe: 'pig', 
+  little: 'chicken'
+};
+const {bessie, dolly, babe, little}= animalNames;
+
+const animalColors = {
+ blackAndWhite: 'cow' ,
+ //bolt : 'horse',
+ black: 'sheep' ,
+  pink: 'pig', 
+ // little: 'chicken'
+};
+const{blackAndWhite, black, pink}= animalColors;
+
+//arrays
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet]= colors;
+const[r, o, y, g, b, , v]= colors;
+const [ , , , , , indg, ]=colors;
 
+//Objects
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +40,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName, color, song, job, partner}= muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,7 +57,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
+const {song2, song4,}= nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner}= nestedMuppet;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
